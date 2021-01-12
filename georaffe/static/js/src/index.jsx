b39c306e816482dc48f5ecae99e44c379f8c4630
john <borges-2014@live.com>
2021-01-11 23:10:04 -0300

    chore: Remove commented code

--- georaffe/static/js/src/index.jsx
+++ georaffe/static/js/src/index.jsx
@@ -5,9 +5,7 @@ import App from './App.jsx';
 
 ReactDOM.render(
   <HashRouter>
-    {/* <BrowserRouter> */}
     <App />
-    {/* </BrowserRouter>, */}
   </HashRouter>,
   document.querySelector('#root'),
 );
